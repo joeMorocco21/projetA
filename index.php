140a28405cc75a8efea92ee93fd00602e07c6635
joeMorocco21 <68898597+joeMorocco21@users.noreply.github.com>
2020-09-03 23:51:41 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+	<title>Audi Q2</title>
+</head>
+
+<body>
+		<noscript>
+		<h1>
+		Activez le JavaScripte sur votre Navigateur pour le bon fonctionnement du site
+		</h1>
+		    <style type="text/css">
+			.container {display:none;}
+			</style>
+		</noscript>
+      <div class="container fill">
+
+      <div class="row">
+
+        <div class="col-lg-12">
+        <?php include_once('menu.php'); ?>
+        </div>
+
+		    <div class="col-lg-12">
+		      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+ 			      <ol class="carousel-indicators">
+   				     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    			     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    			     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    			     <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    			     <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    			     <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+    			     <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+    			     <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+  			    </ol>
+  			    <div class="carousel-inner caroussel-img">
+    			     <div class="carousel-item active">
+     				     <img class="d-block w-100" src="img/image1.jpg" alt="First slide">
+    			     </div>
+    			     <div class="carousel-item">
+      				    <img class="d-block w-100" src="img/photo1.jpg" alt="Second slide">
+    			     </div>
+   				     <div class="carousel-item">
+      				    <img class="d-block w-100" src="img/photo2.jpg" alt="Third slide">
+  				      </div>
+  				      <div class="carousel-item">
+      				    <img class="d-block w-100" src="img/photo3.jpg" alt="Forth slide">
+  				      </div>
+  				      <div class="carousel-item">
+      			 	    <img class="d-block w-100" src="img/photo4.jpg" alt="fifth slide">
+  				      </div>
+  				      <div class="carousel-item">
+      			     	<img class="d-block w-100" src="img/photo5.jpg" alt="sixth slide">
+  				      </div>
+  				      <div class="carousel-item">
+      				    <img class="d-block w-100" src="img/photo6.jpg" alt="seventh slide">
+  				      </div>
+  				      <div class="carousel-item">
+      				    <img class="d-block w-100" src="img/photo7.jpg" alt="eighth slide">
+  				      </div>
+  			     </div>
+ 			        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    			     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    			     <span class="sr-only">Previous</span>
+  			     </a>
+  			     <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    			     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    			     <span class="sr-only">Next</span>
+ 			       </a>
+		      </div>		
+		    </div>
+
+			   <div class="col-lg-12 presentation">
+				  <h1 id="presentation">Un dynamisme à toute épreuve</h1>
+				  <p>De série, l’Audi Q2 est équipée de la direction progressive, qui réduit l’effort de braquage à mesure que vous tournez le volant. En option, la transmission intégrale permanente quattro, allient sportivité et agilité sur toutes les surfaces, en toutes conditions.</p>
+        </div>
+		
+
+			   <div class="col-lg-6 exterieur">
+           <img src="img/image2.jpg">
+        </div>
+        <div class="col-lg-6 exterieur ">
+          <h1 id="exterieur">Un design à la pointe</h1>
+          <p>SUV ? Coupé ? Citadine ? Avec des flancs sculptés, des ailes élargies, une calandre Singleframe caractéristique et des optiques de phares anguleuses, le design de l’Audi Q2 est unique. La ligne de toit plongeante, les prises d’air imposantes et le long becquet de toit sont autant de caractéristiques inspirées des modèles sportifs. La position surélevée et les passages de roues musclés sont, quant à eux, un rappel de l’appartenance de l’Audi Q2 au segment des SUV. Avec une hauteur de 1,51 mètre, une largeur de 1,79 mètre et une longueur de 4,19 mètres, l’Audi Q2 a des proportions idéales pour se faufiler en ville.</p> 
+        </div>
+       
+        <div class="row flex-columm-reverse flex-lg-row moteur-responsive">
+          <div class="col-lg-6 col-sm-6  moteur ">
+            <h1 id="moteur">Caractéristiques techniques</h1>
+             <p>Cylindrée cm3 : 999 cm3</p>
+              <p>Puissance max., kW à tr/mn : 85 (116)/5000-5500</p>
+             <p>Couple max., Nm à tr/mn : 200/2000 - 3500</p>
+              <p>Vitesse maximale : 197 km/h</p>
+              <p>Accélération 0-100 km/h : 10,1 s </p> 
+          </div>
+          <div class="col-lg-6 col-sm-6 moteur">
+           <img src="img/image3.jpg">
+         </div>
+        </div>
+          
+        <div class interieur-responsive>
+        <div class="col-lg-12 interieur1 ">
+          <h1 id="interieur">Le confort d’un SUV</h1>
+         <p>Grâce à un empattement de 2,60 mètres, l’espace à bord de l’Audi Q2 peut accueillir confortablement le conducteur et quatre passagers. Le volume de coffre généreux et la banquette arrière modulable permettent un chargement pouvant aller jusqu’à 1050 litres. A l’intérieur, la position surélevée offre une visibilité propre aux SUV.</p>
+        </div>
+        <div class="col-lg-12 interieur">
+          <img src="img/image4.jpg">
+        </div>
+        </div>
+
+      
+        <div class="col-lg-6 equipement ">
+          <h1 id="equipement">Toutes les informations en ligne de mire</h1>
+         <p>Quelle que soit la finition choisie, l’Audi Q2 est équipée d’un écran MMI de 5,8 pouces. Le système MMI Navigation plus avec MMI Touch (en option) intègre un pavé tactile disposé sur la console centrale. Pour faciliter votre conduite, la commande vocale naturelle vous permet de contrôler de nombreuses fonctionnalités avec votre voix. En option, les instruments analogiques standards peuvent être remplacés par l’Audi virtual cockpit, tableau de bord digital. L’affichage tête haute complète le tout et vient projeter les données de conduite sous la forme de graphiques et de chiffres en couleur sur une vitre située directement dans votre champ de vision.</p> 
+        </div>
+        <div class="col-lg-6 equipement">
+          <img src="img/image5.jpg">
+        </div>
+      
+        <div class="col-lg-6 accessoire">
+          <img src="img/image6.jpg">
+       </div>
+        <div class="col-lg-6 accessoire">
+          <h1 id="accessoire">Connectivité de pointe</h1>
+          <p>Avec le système MMI Navigation, naviguer sur internet et utiliser les services connectés sont encore plus facile grâce à un hotspot wifi intégrant la norme LTE/UMTS. Le forfait nécessaire est inclus directement dans une carte SIM de données intégrée au véhicule (essai gratuit pendant 3 mois), sans que vous n’ayez de manipulation à faire. En utilisant l’Audi smartphone interface, compatible avec les systèmes d’exploitation iOS et Android, vous pouvez afficher les contenus de votre téléphone mobile directement sur l’écran MMI.</p> 
+        </div> 
+
+      
+        <div class="col-lg-4 col-sm-4 col-4 logo">
+          <h2>Rejoinnez-nous</h2>
+           <a href="https://www.facebook.com/audifrance"><i class="fa fa-facebook-official fb"></a></i>
+            <a href="https://twitter.com/audifrance"><i class="fa fa-twitter twitter"></a></i>
+            <a href="https://www.youtube.com/audifrance"><i class="fa fa-youtube youtube"></a></i>
+            <a href="https://www.instagram.com/audifrance"><i class="fa fa-instagram insta"></a></i>
+        </div>
+        <div class="col-lg-4 col-sm-4 col-4 contact">
+          <h2>Contact</h2>
+            <p>Audi France<br>
+            Services Relations Clients<br>
+            11, av. de Boursonne<br>
+            BP 67<br>
+            02601 Villers-Cotterêts Cedex</p>
+        </div>
+        <div class="col-lg-4 col-4 col-sm-4 condition-utilisation">
+          <h2>Conditions générales d'utilisation</h2>
+            <p>Mentions lègales<br>
+            Réglements<br>
+            Conditions générales d'achat<br>
+            Cookies</p>
+         </div>
+      </div>
+    </div>
+          
+</body>
+  
+</html>
